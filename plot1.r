16c8ddde9c0cc1802ac56b3cc6388196c1bcccdb
Robertoavendano <Robertoavendano@hotmail.com>
2014-08-10 18:44:11 -0430

    Create plot1.r

---
+++ plot1.r
@@ -0,0 +1,10 @@
+ table<-read.csv("household_power_consumption.txt",sep=";",as.is = TRUE)
+ table$Date<-as.Date(table$Date, "%d/%m/%Y")
+
+ ElecTab<-table[(table[,1] == "2007-02-02" ) | (table[,1] == "2007-02-01" ), ]
+ ElecTab[,3]<-as.numeric(ElecTab[,3])
+ 
+ hist(ElecTab[,3],col="red",xlab="Global Active Power ( kilowatts ) ",main="Global Active Power")
+ dev.copy(png,file="plot1.png")
+ dev.off()
+
